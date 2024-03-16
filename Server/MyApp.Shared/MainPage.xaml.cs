1b18c2346ff53ef50475bb962632759d23fac45c
root <root@DESKTOP-0C3FQGG>
2024-03-16 23:48:06 +0300

    big bugfixes and adding UDP

--- Server/MyApp.Shared/MainPage.xaml.cs
+++ Server/MyApp.Shared/MainPage.xaml.cs
@@ -20,6 +20,10 @@ using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Globalization;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;  
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -80,181 +84,206 @@ X509Certificate2 CreateSelfSignedCertificate()
     			// Get the IP from GetHostByName method of dns class. 
     			string IP = Dns.GetHostByName(hostName).AddressList[1].ToString(); 
      			Console.WriteLine("IP Address is : " + IP);  
-			IpText.Text = IP+':';
-			PortText.Text = "8081";
+                IpText.Text = IP+':';
+                PortText.Text = "8081";
 		}
 		async void Button1_Click(object sender, RoutedEventArgs e){
 		
-	bool IsWorking = true;
+                bool IsWorking = true;
 
 
-            String IpStr = IpText.Text;
-            String PortStr = PortText.Text;
-            String AllIpPort = IpStr+PortStr;
-if (!QuicListener.IsSupported)
-{
-Console.WriteLine("QUIC is not supported, check for presence of libmsquic and support of TLS 1.3.");
-return;
-}
-    
+                        String IpStr = IpText.Text;
+                        String PortStr = PortText.Text;
+                        String AllIpPort = IpStr+PortStr;
+            if (!QuicListener.IsSupported)
+            {
+            Console.WriteLine("QUIC is not supported, check for presence of libmsquic and support of TLS 1.3.");
+            return;
+            }
+                
 
-while(IsWorking){
-    var cert2 = CreateSelfSignedCertificate();
-    var serverConnectionOptions = new QuicServerConnectionOptions
-    {
-    DefaultStreamErrorCode = 0x0A,
-    DefaultCloseErrorCode = 0x0B,
-    ServerAuthenticationOptions = new SslServerAuthenticationOptions
-    {
-    ApplicationProtocols = new List<SslApplicationProtocol>() { new SslApplicationProtocol("test") },
-    ServerCertificate = cert2,
-    ClientCertificateRequired = false,
-    RemoteCertificateValidationCallback = (sender, chain, certificate, errors) => true,
-    }
-    };
-    var listener = await QuicListener.ListenAsync(new QuicListenerOptions
-    {
-    ListenEndPoint = IPEndPoint.Parse(AllIpPort),
-    ApplicationProtocols = new List<SslApplicationProtocol>() { new SslApplicationProtocol("test") },
-    ConnectionOptionsCallback = (_, _, _) => ValueTask.FromResult(serverConnectionOptions)
-    }); 
-     
-TextBoxConnect.Text = "Connect started...";
-Console.WriteLine(listener.LocalEndPoint);
-    int k = 0;
-    String EndString = null;
-    await using var connection = await listener.AcceptConnectionAsync();
-    await using var stream = await connection.AcceptInboundStreamAsync();
+            while(IsWorking){
+                var cert2 = CreateSelfSignedCertificate();
+                var serverConnectionOptions = new QuicServerConnectionOptions
+                {
+                DefaultStreamErrorCode = 0x0A,
+                DefaultCloseErrorCode = 0x0B,
+                ServerAuthenticationOptions = new SslServerAuthenticationOptions
+                {
+                ApplicationProtocols = new List<SslApplicationProtocol>() { new SslApplicationProtocol("test") },
+                ServerCertificate = cert2,
+                ClientCertificateRequired = false,
+                RemoteCertificateValidationCallback = (sender, chain, certificate, errors) => true,
+                }
+                };
+                var listener = await QuicListener.ListenAsync(new QuicListenerOptions
+                {
+                ListenEndPoint = IPEndPoint.Parse(AllIpPort),
+                ApplicationProtocols = new List<SslApplicationProtocol>() { new SslApplicationProtocol("test") },
+                ConnectionOptionsCallback = (_, _, _) => ValueTask.FromResult(serverConnectionOptions)
+                }); 
+                
+            TextBoxConnect.Text = "Connect started QUIC...";
+            Console.WriteLine(listener.LocalEndPoint);
+                int k = 0;
+                String EndString = null;
+                await using var connection = await listener.AcceptConnectionAsync();
+                await using var stream = await connection.AcceptInboundStreamAsync();
 
-    Console.WriteLine("Begin connection");
-	TextBoxConnect.Text = "Client connected with " + listener.LocalEndPoint;
-    // Read
-    String masaggeToString = null;
-    var buffer = new byte[1024];
+                Console.WriteLine("Begin connection");
+                TextBoxConnect.Text = "Client connected with " + listener.LocalEndPoint;
 
-    
-    try{
-        int BufferSizeCons = Int32.Parse(BufferSize.Text); 
-        buffer = new byte[BufferSizeCons];
-    }catch{}
+                // Read
+                String masaggeToString = null;
+                var buffer = new byte[1024];
 
-           FileText.Text = " ";
-           int bufferCount = 0;
-           int memorySize = 0;
-           int indexOfConsoleLog = 0;
-           DateTime localDate_start = DateTime.Now;
-           ConsoleText.Text = " ";
-           byte[] tmpBuffer = new byte[0];
-           byte[] byteName = new byte[0];
-           List<string> name = new List<string>();
-           String result_name = "";
-           string cleanName = "";
-           bool isNotNamed = true;
-            int b = 0;
-           FileStream sw = new FileStream("text.txt", FileMode.Create);
+                
+                try{
+                    int BufferSizeCons = Int32.Parse(BufferSize.Text); 
+                    buffer = new byte[BufferSizeCons];
+                }catch{}
 
-    while(await stream.ReadAsync(buffer) > 0){
-        bufferCount++;
-        Console.WriteLine("Buffer #"+bufferCount);
-        int tmpIntVal = 0;
-        while( b < buffer.Length){
-            
-                    if(buffer[b] == 36 && buffer[b+1] == 36){
-                        // Console.WriteLine(Encoding.UTF8.GetString(byteName, 0, byteName.Length));
-                        // break;
-                        b = buffer.Length;
-                    }else{
-                        byteName = new byte[1];
-                        byteName[0] = buffer[b];
-                        Console.WriteLine(Encoding.UTF8.GetString(byteName, 0, byteName.Length));
-                        result_name += Encoding.UTF8.GetString(byteName, 0, byteName.Length);
+                    FileText.Text = " ";
+                    int bufferCount = 0;
+                    int memorySize = 0;
+                    int indexOfConsoleLog = 0;
+                    DateTime localDate_start = DateTime.Now;
+                    ConsoleText.Text = " ";
+                    byte[] tmpBuffer = new byte[0];
+                    byte[] byteName = new byte[0];
+                    List<string> name = new List<string>();
+                    String result_name = "";
+                    string cleanName = "";
+                    bool isNotNamed = true;
+                        int b = 0;
+                        int bb = 0;
+                    FileStream sw = new FileStream("text.txt", FileMode.Create);
+
+                while(await stream.ReadAsync(buffer) > 0){
+
+                    DateTime localDate_buffer = DateTime.Now;
+                    bufferCount++;
+                    Console.WriteLine("Buffer #"+bufferCount);
+                    int tmpIntVal = 0;
+
+                    if(isNotNamed){
+
+                        while( b < buffer.Length){
+                            
+                                    if(buffer[b] == 36 && buffer[b+1] == 36){
+                                        // Console.WriteLine(Encoding.UTF8.GetString(byteName, 0, byteName.Length));
+                                        // break;
+                                        bb = b + 2;
+                                        Console.WriteLine(bb);
+                                        b = buffer.Length;
+                                    }else{
+                                        byteName = new byte[1];
+                                        byteName[0] = buffer[b];
+                                        Console.WriteLine(Encoding.UTF8.GetString(byteName, 0, byteName.Length));
+                                        result_name += Encoding.UTF8.GetString(byteName, 0, byteName.Length);
+                                    }
+                                    b++;
+                        }
+                        sw = new FileStream(result_name, FileMode.Create);
+                        isNotNamed = false;
+                        Console.WriteLine("named");
                     }
-                    b++;
-        }
-        if(isNotNamed){
-            sw = new FileStream(result_name, FileMode.Create);
-            isNotNamed = false;
-            Console.WriteLine("named");
-        }
-        for(int l = 0; l < buffer.Length; l++){
+                    for(int l = +bb; l < buffer.Length; l++){
+                        try{
+                            if((buffer[l] == 115) && (buffer[l+1] == 116) && (buffer[l+2] == 111) && (buffer[l+3] == 112) && (buffer[l+4] == 36) && (buffer[l+5] == 36)){
+                            
+                                break;
+                            }
+                            else{
+                                tmpIntVal++;
+                                tmpBuffer = new byte[tmpIntVal];
+                            }
+                        }catch(Exception jk){
+                            Console.WriteLine(jk);
+                        }
+                    }
+                    
+                    DateTime localDate_buufer_end = DateTime.Now;
+                    TimeSpan localDate_buffer_value = localDate_buffer.Subtract(localDate_buufer_end);
+                    Console.WriteLine("new buffer length "+tmpBuffer.Length + " time end: " + localDate_buffer_value);
+                    if(indexOfConsoleLog < 5){
+                        ConsoleText.Text += "Новая длина буффера: "+tmpBuffer.Length+'\n';
+                    }else{
+                        ConsoleText.Text = " ";
+                        indexOfConsoleLog = 0;
+                    }
+                    indexOfConsoleLog += 1;
+                    for(int y = 0; y < tmpBuffer.Length; y++){
+                        tmpBuffer[y] = buffer[y+bb];
+                    }
 
+                    memorySize+=tmpBuffer.Length;
+                    ReadedText.Text = Encoding.UTF8.GetString(tmpBuffer, 0, tmpBuffer.Length);
 
-            if((buffer[l] == 115) && (buffer[l+1] == 116) && (buffer[l+2] == 111) && (buffer[l+3] == 112) && (buffer[l+4] == 36) && (buffer[l+5] == 36)){
-             
-                break;
+                    char[] charsToTrim = {' '};
+                    cleanName = result_name.Trim(charsToTrim);
+                    
+                    sw.Write(tmpBuffer, 0, tmpBuffer.Length);
+
+                    bb = 0;
+                }
+                    sw.Close();
+
+                //ReadedText.Text = EndString;
+                Console.WriteLine("readed buffer");
+                Console.WriteLine(memorySize);
+                DateTime localDate_end = DateTime.Now;
+                TimeSpan localDate = localDate_start.Subtract(localDate_end);
+                //string Cult = "ru-RU";
+                //var culture = new CultureInfo(Cult);
+                //string time = localDate.ToString(culture);
+
+                FileText.Text = "Размер файла: "+ memorySize + " байтов" + ", отправка заняла: " + localDate.Minutes + " Минут " + localDate.Seconds + " Секунд " + localDate.Milliseconds + " Милисекунд\n " + " файл с названием "+ cleanName +" сохранен в эту деррикторию " + ". Количество принятых блоков: " + bufferCount;
+                // Console.WriteLine("readed");
+                try
+                {
+                    await listener.DisposeAsync();
+
+                }
+                catch(Exception ee)
+                {
+                    Console.WriteLine("Exception: " + ee.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Executing finally block.");
+                }
+                k++;
+                //if(k == 100){
+                //    IsWorking = false;
+                //}
             }
-            else{
-                tmpIntVal++;
-                tmpBuffer = new byte[tmpIntVal];
-            }
-        }
-        Console.WriteLine("new buffer length "+tmpBuffer.Length);
-        if(indexOfConsoleLog < 5){
-            ConsoleText.Text += "Новая длина буффера: "+tmpBuffer.Length+'\n';
-        }else{
-            ConsoleText.Text = " ";
-            indexOfConsoleLog = 0;
-        }
-        indexOfConsoleLog += 1;
-        for(int y = 0; y < tmpBuffer.Length; y++){
-            tmpBuffer[y] = buffer[y];
-        }
 
-
-        memorySize+=tmpBuffer.Length;
-        ReadedText.Text = Encoding.UTF8.GetString(tmpBuffer, 0, tmpBuffer.Length);
-
-        char[] charsToTrim = {' '};
-        cleanName = result_name.Trim(charsToTrim);
-
-        // Console.WriteLine(cleanName.Length);
-
-
-        
-        sw.Write(tmpBuffer, 0, tmpBuffer.Length);
-
-
-    }
-        sw.Close();
-
-    //ReadedText.Text = EndString;
-    Console.WriteLine("readed buffer");
-	Console.WriteLine(memorySize);
-    DateTime localDate_end = DateTime.Now;
-    TimeSpan localDate = localDate_start.Subtract(localDate_end);
-    //string Cult = "ru-RU";
-    //var culture = new CultureInfo(Cult);
-    //string time = localDate.ToString(culture);
-
-    FileText.Text = "Размер файла: "+ memorySize + " байтов" + ", отправка заняла: " + localDate.Minutes + " Минут " + localDate.Seconds + " Секунд " + localDate.Milliseconds + " Милисекунд\n " + " файл с названием "+ cleanName +" сохранен в эту деррикторию " + ". Количество принятых блоков: " + bufferCount;
-	Console.WriteLine("readed");
-    EndString = EndString + masaggeToString;
-    Console.WriteLine(EndString.Length);
-    try
-    {
-        await listener.DisposeAsync();
-
-    }
-    catch(Exception ee)
-    {
-        Console.WriteLine("Exception: " + ee.Message);
-    }
-    finally
-    {
-        Console.WriteLine("Executing finally block.");
-    }
-    k++;
-    //if(k == 100){
-    //    IsWorking = false;
-    //}
-}
-
-    Console.WriteLine("Exit");
+            Console.WriteLine("Exit");
 
 		}
         async void Button3_Click(object sender, RoutedEventArgs e){
             ConsoleText.Text = " ";
             ReadedText.Text = " ";
+        }
+
+        async void Button4_Click(object sender, RoutedEventArgs e){
+            Console.WriteLine("UDP is upped");    
+            TextBoxConnect.Text = "Connect started UDP...";
+            var receiveClient = new UdpClient();
+            receiveClient.EnableBroadcast = true;
+            receiveClient.Client.Bind(new IPEndPoint(IPAddress.Parse(IpText.Text.Remove(IpText.Text.Length-1, 1)), Int32.Parse(PortText.Text)));
+
+            while (true)
+            {
+                var result = await receiveClient.ReceiveAsync();
+                string message = Encoding.UTF8.GetString(result.Buffer);
+                if (message == "END") break;
+                Console.WriteLine(message);
+                
+            }
+            
+            
         }
 	}
 }
